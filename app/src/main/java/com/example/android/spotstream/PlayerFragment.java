3134bc0203f418503abdeb93e566d5b10c8fdedb
matt31415 <matthew.gay@gmail.com>
2015-08-19 23:14:28 -0400

    Prev/Next buttons are working.

--- app/src/main/java/com/example/android/spotstream/PlayerFragment.java
+++ app/src/main/java/com/example/android/spotstream/PlayerFragment.java
@@ -1,30 +1,134 @@
 package com.example.android.spotstream;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class PlayerFragment extends Fragment {
 
+    private MediaPlayer mMediaPlayer;
+    private MediaPlayerState mMediaPlayerState;
+    private ArrayList<Song> mSongList;
+    private int mCurrSongPosition;
+
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
+
+    private enum MediaPlayerState {
+        IDLE,
+        PREPARED,
+        STARTED,
+        PAUSED,
+        STOPPED
+    }
+
+    private enum Direction {
+        NEXT,
+        PREV
+    }
+
     public PlayerFragment() {
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayerState = MediaPlayerState.IDLE;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_player, container, false);
+        final View view = inflater.inflate(R.layout.fragment_player, container, false);
 
-        Song song = (Song)getActivity().getIntent().getSerializableExtra(getString(R.string.player_songs_key));
+        mSongList = (ArrayList<Song>) getActivity().getIntent().getSerializableExtra(getString(R.string.player_songs_key));
+        mCurrSongPosition = getActivity().getIntent().getIntExtra(getString(R.string.player_song_position_key), 0);
 
+        recreateView(view);
+
+        final ImageButton playPauseButton = (ImageButton) view.findViewById(R.id.player_play_pause_button);
+        playPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mMediaPlayerState == MediaPlayerState.IDLE) {
+                    // This code was heavily influenced by:
+                    // http://stackoverflow.com/questions/23309857/android-correct-usage-of-prepareasync-in-media-player-activity
+                    try {
+                        mMediaPlayer.setDataSource(mSongList.get(mCurrSongPosition).mPreviewUrl);
+                        mMediaPlayer.prepareAsync();
+                        playPauseButton.setImageDrawable(getActivity().getDrawable(android.R.drawable.ic_media_pause));
+                    } catch (IOException e) {
+                        Toast.makeText(view.getContext(), "Track preview not found", Toast.LENGTH_SHORT).show();
+                    }
+
+                    mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            mMediaPlayerState = MediaPlayerState.PREPARED;
+                            mp.start();
+                            mMediaPlayerState = MediaPlayerState.STARTED;
+                            playPauseButton.setImageDrawable(getActivity().getDrawable(android.R.drawable.ic_media_pause));
+                        }
+                    });
+                } else if (mMediaPlayerState == MediaPlayerState.STARTED) {
+                    mMediaPlayer.pause();
+                    mMediaPlayerState = MediaPlayerState.PAUSED;
+                    playPauseButton.setImageDrawable(getActivity().getDrawable(android.R.drawable.ic_media_play));
+                } else if (mMediaPlayerState == MediaPlayerState.PAUSED) {
+                    mMediaPlayer.start();
+                    mMediaPlayerState = MediaPlayerState.STARTED;
+                    playPauseButton.setImageDrawable(getActivity().getDrawable(android.R.drawable.ic_media_pause));
+                }
+            }
+        });
+
+        mNextButton = (ImageButton) view.findViewById(R.id.player_next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeSong(v, Direction.NEXT);
+            }
+        });
+
+        mPrevButton = (ImageButton) view.findViewById(R.id.player_prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeSong(v, Direction.PREV);
+            }
+        });
+
+        return view;
+    }
+
+
+    /**
+     * Redraws (or draws for the first time) the view based on the value of mCurrSongPosition
+     */
+    private void recreateView() {
+        recreateView(getView());
+    }
+
+    /**
+     * Redraws (or draws for the first time) the view based on the value of mCurrSongPosition
+     *
+     * @param view
+     */
+    private void recreateView(View view) {
+        Song song = mSongList.get(mCurrSongPosition);
         TextView artistText = (TextView) view.findViewById(R.id.player_artist_text);
         artistText.setText(song.mArtist);
 
@@ -51,6 +155,59 @@ public class PlayerFragment extends Fragment {
                     .into(songImageView);
         }
 
-        return view;
+        return;
+    }
+
+    private void changeSong(View view, Direction direction) {
+        if (direction == Direction.NEXT)
+            mCurrSongPosition++;
+        else if (direction == Direction.PREV) {
+            mCurrSongPosition--;
+        }
+
+        recreateView();
+
+        // Enable and disable the next/previous buttons as appropriate
+        if(mCurrSongPosition == mSongList.size() - 1) {
+            mNextButton.setEnabled(false);
+            mNextButton.setImageAlpha(50);
+        }
+        else if(mCurrSongPosition == mSongList.size() - 2) {
+            mNextButton.setEnabled(true);
+            mNextButton.setImageAlpha(255);
+        }
+        else if(mCurrSongPosition == 0) {
+            mPrevButton.setEnabled(false);
+            mPrevButton.setImageAlpha(50);
+        }
+        else if(mCurrSongPosition == 1) {
+            mPrevButton.setEnabled(true);
+            mPrevButton.setImageAlpha(255);
+        }
+
+        if (mMediaPlayerState == MediaPlayerState.IDLE || mMediaPlayerState == MediaPlayerState.PAUSED) {
+            mMediaPlayer.reset();
+            mMediaPlayerState = MediaPlayerState.IDLE;
+        } else if (mMediaPlayerState == MediaPlayerState.STARTED) {
+            mMediaPlayer.reset();
+            mMediaPlayerState = MediaPlayerState.IDLE;
+            try {
+                mMediaPlayer.setDataSource(mSongList.get(mCurrSongPosition).mPreviewUrl);
+                mMediaPlayer.prepareAsync();
+            } catch (IOException e) {
+                Toast.makeText(view.getContext(), "Track preview not found", Toast.LENGTH_SHORT).show();
+            }
+
+            mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mMediaPlayerState = MediaPlayerState.PREPARED;
+                    mp.start();
+                    mMediaPlayerState = MediaPlayerState.STARTED;
+                }
+            });
+
+        }
+
     }
 }
